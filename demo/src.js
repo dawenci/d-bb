f5565dd196d29c3c68bfae64901cfe602e62c2d1
Davin Chang Juan <dawenci@gmail.com>
2016-02-12 12:33:11 +0800

    添加初始文件

---
+++ demo/src.js
@@ -0,0 +1,1 @@
+var Dbb = window.Dbb = require('../index.js');
---
+++ index.js
@@ -0,0 +1,10 @@
+var Dbb = require('./lib/dbb');
+require('./lib/view');
+require('./lib/collection-view');
+require('./lib/item-view');
+require('./lib/scroll-view');
+require('./lib/image-view');
+require('./lib/view-manager');
+require('./lib/helpers');
+
+module.exports = Dbb;
