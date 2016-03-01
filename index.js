a7de9bd12539be0f3044f0231277bc0509eb63e2
Davin Chang Juan <dawenci@gmail.com>
2016-03-02 00:50:35 +0800

    新增控制器类、控制器管理类

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ require('./lib/collection-view');
 require('./lib/item-view');
 require('./lib/scroll-view');
 require('./lib/controller');
+require('./lib/controller-manager');
 require('./lib/helpers');
 
 module.exports = Dbb;
