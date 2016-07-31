27d5589ba43efdddda2ac4d429b91ab507f8e2b8
Davin Chang Juan <dawenci@gmail.com>
2016-08-01 01:53:45 +0800

    viewForItem 新增传入collection参数

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ Dbb.View = require('./lib/view');
 Dbb.CollectionView = require('./lib/collection-view');
 Dbb.Controller = require('./lib/controller');
 Dbb.ControllerManager = require('./lib/controller-manager');
+window.Dbb = Dbb;
 
 module.exports = Dbb;
